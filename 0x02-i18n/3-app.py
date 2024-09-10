56ef658202dac8c1bda4e6a2577aef599a4ee2e4
Ahadi <ahadic044@gmail.com>
2024-09-10 20:31:01 +0200

    correcting code implementation

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-"""a simple flask app initialized wwitha simple htmls file"""
-
-
+"""A Basic Flask app with internationalization support.
+"""
+from flask_babel import Babel
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
 
 
-class Config():
-    ''' A configuration class file'''
-
+class Config:
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -17,28 +16,22 @@ class Config():
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
-
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
-    '''a decorated function to retrieve the locale'''
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
-def home() -> str:
-    '''a simplel page that displays a title and a header'''
-
-    home_title = gettext("home_title")
-    home_header = gettext("home_header")
-
-    return render_template(
-        '3-index.html',
-        home_title=home_title,
-        home_header=home_header)
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
